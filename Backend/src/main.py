492f33b2a86e6be3407b5d1c6651cda1bfc95df7
premkeshava <premkeshava@Mac.lan>
2025-11-06 01:27:24 +0530

    made the change of constants module to src.constants to fix the deployment issue.

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from constants import ALLOWED_ORIGINS
+from src.constants import ALLOWED_ORIGINS
 
 app = FastAPI(title="Hotel-IQ")
 
